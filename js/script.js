1c8453c35b5fdde5d516ead16153f512f5615640
chandana-46 <rampuramchandu7@gmail.com>
2025-11-26 15:27:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
-// Smooth Scroll for Navigation
-document.querySelectorAll('nav ul li a').forEach(link => {
-    link.addEventListener("click", e => {
-        e.preventDefault();
-        const section = document.querySelector(e.target.getAttribute("href"));
-        section.scrollIntoView({ behavior: "smooth" });
-    });
-});
+function sendMessage() {
+    let name = document.getElementById("name").value;
+    let msg = document.getElementById("message").value;
+
+    if (name === "" || msg === "") {
+        alert("Please fill out all fields.");
+        return;
+    }
+
+    alert("Thank you, " + name + "! Your message has been sent.");
+}
